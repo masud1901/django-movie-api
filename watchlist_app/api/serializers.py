192afdd49df9dfe97816ef8c594c465386d833c8
masud1901 <akmolmasud5@gmail.com>
2024-01-25 21:32:19 +0600

    created 1 to many relationship with platform to watch and reinitialized the database

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -10,6 +10,7 @@ class WatchListSerializer(serializers.ModelSerializer):
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
     class Meta:
         model = StreamPlatform
         fields = "__all__"
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -6,12 +6,17 @@ class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
     about = models.TextField(max_length=150)
     website = models.URLField(max_length=100)
+
     def __str__(self) -> str:
         return self.name
+
 
 class WatchList(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.TextField(max_length=200)
+    platform = models.ForeignKey(
+        StreamPlatform, on_delete=models.CASCADE, related_name="watchlist"
+    )
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
