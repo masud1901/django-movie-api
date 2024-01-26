11c11387154e14e0da37373e55b337760b9b8b91
masud1901 <akmolmasud5@gmail.com>
2024-01-26 15:23:15 +0600

    used generic view and mixins class for review section

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path, include
-from watchlist_app.api.views import StreamPlatformAV, WatchListAV, WatchListDetailsAV,StreamPlatformDetailAV
+from watchlist_app.api.views import (
+    StreamPlatformAV,
+    WatchListAV,
+    WatchListDetailsAV,
+    StreamPlatformDetailAV,
+    ReviewList,
+    ReviewDetail
+)
 
 urlpatterns = [
     path("list/", WatchListAV.as_view(), name="watchlist"),
     path("<int:pk>", WatchListDetailsAV.as_view(), name="watchlist-details"),
     path("stream/", StreamPlatformAV.as_view(), name="stream"),
     path("stream/<int:pk>", StreamPlatformDetailAV.as_view(), name="stream-details"),
+    path("review/", ReviewList.as_view(), name="review-list"),
+    path("review/<int:pk>", ReviewDetail.as_view(), name="review-details"),
 ]
